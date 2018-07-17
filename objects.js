fec53ed3f1c64e9dfb921b23e0c9fcd078aaab64
kevinyhong <kevinyaohuiong@gmail.com>
2018-07-17 17:14:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var playlist = new Object({artistName: 'songTitle'})
 
 function updatePlaylist(playlist, artistName, songTitle) {
@@ -6,4 +7,15 @@ function updatePlaylist(playlist, artistName, songTitle) {
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
+=======
+var playlist = {}
+
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist.artistName = songTitle
+  return playlist
+}
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+>>>>>>> dc9775f8f1a52745c63ca967b0c10ace850ab81e
 }
